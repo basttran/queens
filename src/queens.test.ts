e8fbcc3adc831cc42a757aa8e6c9dc35d44e3aeb
Bastien Tran <bastien.tran@comet.team>
2024-01-26 13:48:33 +0100

    supergreen

--- src/queens.test.ts
+++ src/queens.test.ts
@@ -1,76 +1,70 @@
-import * as NEA from 'fp-ts/NonEmptyArray';
-import * as A from 'fp-ts/Array';
-import { pipe } from 'fp-ts/function';
-import { describe, expect, expectTypeOf, it } from "vitest";
-
-const solveNQueens = (nbQueens: number): Array<PositionsForQueens> => {
-  const column: Col = nbQueens;
-  
-  const firstRow: Array<PositionsForQueens> = []
-
-  for (let i = 0 ; i < column; i++ ) {
-    firstRow.push([i])
-  }
-
-  const secondRow: Array<PositionsForQueens> = [];
-
-  firstRow.forEach(solution => {
-    for(let j= 0; j < column; j++ ) {
-      if(solution[0] === j) {
-        continue
-      }
-      secondRow.push([solution[0], j])
-    }
-  })
-  return secondRow
-  
-
-
-  // First Queen déplacer sur toutes les colonnes 
-  // ==> 
-  // [[0], [1]]
-  // [[0, 1], [1, 0]]
-  // If Second Queen quelles solutions possibles
-  // Sol 1 Queen 1 
-  // Queen 2 col 0 => False 
-  // Queen 2 col 1 => Vrai  ==> Solitions possibles 
-  // [[0, 0], [0, 1]]
-  //  Sol 2 Queen 1 
-  // Queen 2 col 0 => Vrai  ==> Solitions possibles  
-  // Queen 2 col 1 => False
-  //[[0, 1 ], [1, 0]]
-
-
-  // [[0], [1], [2]]
-  // [[[0, 1], [0, 2]], [[1, 0], [1,2], [[2, 0], [2, 1]]]]
-  // [[0, 1], [0, 2], [1, 0], [1,2]]
-
-
-  
-};
-
-type Col = number;
-type PositionsForQueens = Array<Col>;
-
-describe('N queens problem', () => {
-
-  it('should', () => {
-    solveNQueens(4); //?
-    
-  });
-});
+import { describe, expect, it } from "vitest";
 
 
 describe('', () => {
-  it('Should find 2 solutions for two queens given ingoring diagonals', () => {
+  it('Should find 2 solutions for two queens given ignoring diagonals', () => {
+
+    //When
+    const result = solveNQueens(1) //?
+    //Then
+    expect(result).toHaveLength(1)
+  })
+  it('Should find 2 solutions for two queens given ignoring diagonals', () => {
 
     //When
     const result = solveNQueens(2) //?
     //Then
     expect(result).toHaveLength(2)
   })
+  it('Should find  solutions for three queens given ignoring diagonals', () => {
+
+    //When
+    const result = solveNQueens(3) //?
+    //Then
+    expect(result).toHaveLength(6)
+  })
   //it('should detect that two queens are on the same diagonal', () => {})
 })
+
+const solveNQueens = (nbQueens: Col): Array<PositionsForQueens> => {
+  const column: Col = nbQueens;
+  
+
+
+  const result = generateRow(nbQueens);
+
+  return result;
+
+};
+
+const generateRow = (column: number, previous?: PositionsForQueens[]): PositionsForQueens[] => {
+  const firstRow: Array<PositionsForQueens> = []
+  for (let i = 0 ; i < column; i++ ) {
+    firstRow.push([i])
+  }
+  if (firstRow.every((solution) => solution.length === column)) {
+    return firstRow;
+  }
+  const next: Array<PositionsForQueens> = [];
+  (previous ? previous : firstRow).forEach(solution => {
+    for (let j = 0; j < column; j++) {
+      if (solution.indexOf(j) > -1) {
+        continue;
+      }
+      next.push(solution.concat([j]));
+    }
+  });
+  console.log('next[0]: ', next[0]);
+  if (next[0] && next[0].length < column) {
+    return generateRow(column, next)
+  } else {
+    return next;
+  }
+}
+
+type Col = number;
+type PositionsForQueens = Array<Col>;
+
 
 //////////////////////////
 //[ [ 1, 3, 0, 2 ], [ 2, 0, 3, 1 ] ]
