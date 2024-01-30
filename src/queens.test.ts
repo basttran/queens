6c641108a26c03711b0b3e7a9e73d32964bb49b4
Bastien Tran <bastien.tran@comet.team>
2024-01-30 22:30:47 +0100

    working recursion question expects for 0 and 1 queen

--- src/queens.test.ts
+++ src/queens.test.ts
@@ -1,39 +1,36 @@
 import { describe, expect, it } from "vitest";
 
 describe('', () => {
-  // it('Should find 2 solutions for two queens given ignoring diagonals', () => {
-
-  //   //When
-  //   const result = solveNQueens(0) //?
-  //   //Then
-  //   expect(result).toHaveLength(0)
-  // })
-  // it('Should find 2 solutions for two queens given ignoring diagonals', () => {
-
-  //   //When
-  //   const result = solveNQueens(1) //?
-  //   //Then
-  //   expect(result).toHaveLength(1)
-  // })
-  // it('Should find 2 solutions for two queens given ignoring diagonals', () => {
-
-  //   //When
-  //   const result = solveNQueens(2) //?
-  //   //Then
-  //   expect(result).toHaveLength(2)
-  //   expect(result[0]).toHaveLength(2)
-  //   expect(result[1]).toHaveLength(2)
-  // })
-  // it.only('Should find solutions for three queens given ignoring diagonals', () => {
-  //   //When
-  //   const result = solveNQueens(3) //?
-  //   //Then
-  //   expect(result).toHaveLength(6)
-  // })
-  it('Should find  solutions for 4 queens given ignoring diagonals', () => {
+  it('Should find 0 solutions for zero queens given ignoring diagonals', () => {
 
     //When
+    const result = solveNQueens(0) //?
+    //Then
+    expect(result).toHaveLength(0)
+  })
+  it('Should find 1 solutions for one queens given ignoring diagonals', () => {
+
+    //When
+    const result = solveNQueens(1) //?
+    //Then
+    expect(result).toHaveLength(1)
+  })
+  it('Should find 0 solutions for two queens given ignoring diagonals', () => {
+    //When
+    const result = solveNQueens(2) //?
+    //Then
+    expect(result).toHaveLength(0)
+  })
+  it('Should find 0 solutions for three queens given ignoring diagonals', () => {
+    //When
+    const result = solveNQueens(3) //?
+    //Then
+    expect(result).toHaveLength(0)
+  })
+  it('Should find  solutions for 4 queens given ignoring diagonals', () => {
+    //When
     const result = solveNQueens(4) //?
+    console.log('result: ', result);
     //Then
     expect(result).toHaveLength(2)
   })
@@ -46,44 +43,48 @@ const solveNQueens = (nbQueens: Col): Array<PositionsForQueens> => {
 };
 
 const generateRow = (nbQueens: Col, previousSolutions: PositionsForQueens[] = [[]]): PositionsForQueens[] => {
-  const nextRowColumnns : PositionsForQueens[] = [[]];
-  for (let i = 0; i < nbQueens; i++) {
-    nextRowColumnns.push([i])
+  const nextRowColumnns: PositionsForQueens[] = generateNextRowPositionsToTry(nbQueens);
+  const validSolutionsForCurrentIteration = buildValidSolutions(previousSolutions, nextRowColumnns);
+
+  if (validSolutionsForCurrentIteration.length === 0) {
+    return [];
   }
-  const solutionsValidOrNot = buildSolutions(previousSolutions, nextRowColumnns)
-  if (hasSolutionsWithAllQueensUsed(solutionsValidOrNot, nbQueens)) {
-    return keepSolutionsWithAllQueens(solutionsValidOrNot,nbQueens)
+  if (validSolutionsForCurrentIteration.some((solution) => solution.length === nbQueens)) {
+    return validSolutionsForCurrentIteration.filter((solution) => solution.length === nbQueens)
   }
-  return generateRow(nbQueens, solutionsValidOrNot);
+  return generateRow(nbQueens, validSolutionsForCurrentIteration)
 }
 
-const keepSolutionsWithAllQueens = (solutions: number[][], nbQueens: number): PositionsForQueens[] => {
-  return solutions.filter((solution) => solution.length && solution.length === nbQueens);
-}
+const buildValidSolutions = (previousSolutions: PositionsForQueens[], nextRowColumnns: PositionsForQueens[]) => {
 
-const hasSolutionsWithAllQueensUsed = (solutions: number[][], nbQueens: number) => {
-  return solutions.some((nextPosition) => nextPosition.length === nbQueens);
-}
-
-const buildSolutions = (previousSolutions: PositionsForQueens[], nextRowColumnns: PositionsForQueens[]) => {
   return previousSolutions
     .flatMap((solution) => nextRowColumnns
-      .map((next) => solution.indexOf(next[0]) === -1 && !isIntersectingOtherQueensDiagonal(solution, next[0]) ? solution.concat(next) : []));
+      .map((next) => !isOnSameColumnAsPreviousQueens(solution, next) && !isIntersectingOtherQueensDiagonal(solution, next[0]) ?
+      solution.concat(next) :
+      []))
+    .filter((solution) => solution.length >= previousSolutions[0].length);
 }
 
 const isIntersectingOtherQueensDiagonal = (solution: PositionsForQueens, next: Col) => {
-  const solutionLength = solution.length
-  const plop = solution.some((position, index) => {
-    return Math.abs(next - position) === (solutionLength - index)
+  return solution.some((position, index) => {
+    return Math.abs(next - position) === (solution.length - index)
   })
-  return plop;
+}
+
+const isOnSameColumnAsPreviousQueens = (solution: PositionsForQueens, next: PositionsForQueens) => {
+  return solution.indexOf(next[0]) >= 0;
+}
+
+const generateNextRowPositionsToTry = (nbQueens: number) => {
+  const nextRowColumnns: PositionsForQueens[] = [];
+  for (let i = 0; i < nbQueens; i++) {
+    nextRowColumnns.push([i]);
+  }
+  return nextRowColumnns;
 }
 
 type Col = number;
 type PositionsForQueens = Array<Col>;
-
-
-
 
 //////////////////////////
 //[ [ 1, 3, 0, 2 ], [ 2, 0, 3, 1 ] ]
