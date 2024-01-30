fb064c2fb9156dc94963497cb965be67ecb2356b
Bastien Tran <bastien.tran@comet.team>
2024-01-30 13:59:03 +0100

    TODO better exit condition

--- src/queens.test.ts
+++ src/queens.test.ts
@@ -1,69 +1,88 @@
 import { describe, expect, it } from "vitest";
 
-
 describe('', () => {
-  it('Should find 2 solutions for two queens given ignoring diagonals', () => {
+  // it('Should find 2 solutions for two queens given ignoring diagonals', () => {
+
+  //   //When
+  //   const result = solveNQueens(0) //?
+  //   //Then
+  //   expect(result).toHaveLength(0)
+  // })
+  // it('Should find 2 solutions for two queens given ignoring diagonals', () => {
+
+  //   //When
+  //   const result = solveNQueens(1) //?
+  //   //Then
+  //   expect(result).toHaveLength(1)
+  // })
+  // it('Should find 2 solutions for two queens given ignoring diagonals', () => {
+
+  //   //When
+  //   const result = solveNQueens(2) //?
+  //   //Then
+  //   expect(result).toHaveLength(2)
+  //   expect(result[0]).toHaveLength(2)
+  //   expect(result[1]).toHaveLength(2)
+  // })
+  // it.only('Should find solutions for three queens given ignoring diagonals', () => {
+  //   //When
+  //   const result = solveNQueens(3) //?
+  //   //Then
+  //   expect(result).toHaveLength(6)
+  // })
+  it('Should find  solutions for 4 queens given ignoring diagonals', () => {
 
     //When
-    const result = solveNQueens(1) //?
-    //Then
-    expect(result).toHaveLength(1)
-  })
-  it('Should find 2 solutions for two queens given ignoring diagonals', () => {
-
-    //When
-    const result = solveNQueens(2) //?
+    const result = solveNQueens(4) //?
     //Then
     expect(result).toHaveLength(2)
-  })
-  it('Should find  solutions for three queens given ignoring diagonals', () => {
-
-    //When
-    const result = solveNQueens(3) //?
-    //Then
-    expect(result).toHaveLength(6)
   })
   //it('should detect that two queens are on the same diagonal', () => {})
 })
 
 const solveNQueens = (nbQueens: Col): Array<PositionsForQueens> => {
-  const column: Col = nbQueens;
-  
-
-
   const result = generateRow(nbQueens);
-
   return result;
-
 };
 
-const generateRow = (column: number, previous?: PositionsForQueens[]): PositionsForQueens[] => {
-  const firstRow: Array<PositionsForQueens> = []
-  for (let i = 0 ; i < column; i++ ) {
-    firstRow.push([i])
+const generateRow = (nbQueens: Col, previousSolutions: PositionsForQueens[] = [[]]): PositionsForQueens[] => {
+  const nextRowColumnns : PositionsForQueens[] = [[]];
+  for (let i = 0; i < nbQueens; i++) {
+    nextRowColumnns.push([i])
   }
-  if (firstRow.every((solution) => solution.length === column)) {
-    return firstRow;
+  const solutionsValidOrNot = buildSolutions(previousSolutions, nextRowColumnns)
+  if (hasSolutionsWithAllQueensUsed(solutionsValidOrNot, nbQueens)) {
+    return keepSolutionsWithAllQueens(solutionsValidOrNot,nbQueens)
   }
-  const next: Array<PositionsForQueens> = [];
-  (previous ? previous : firstRow).forEach(solution => {
-    for (let j = 0; j < column; j++) {
-      if (solution.indexOf(j) > -1) {
-        continue;
-      }
-      next.push(solution.concat([j]));
-    }
-  });
-  console.log('next[0]: ', next[0]);
-  if (next[0] && next[0].length < column) {
-    return generateRow(column, next)
-  } else {
-    return next;
-  }
+  return generateRow(nbQueens, solutionsValidOrNot);
+}
+
+const keepSolutionsWithAllQueens = (solutions: number[][], nbQueens: number): PositionsForQueens[] => {
+  return solutions.filter((solution) => solution.length && solution.length === nbQueens);
+}
+
+const hasSolutionsWithAllQueensUsed = (solutions: number[][], nbQueens: number) => {
+  return solutions.some((nextPosition) => nextPosition.length === nbQueens);
+}
+
+const buildSolutions = (previousSolutions: PositionsForQueens[], nextRowColumnns: PositionsForQueens[]) => {
+  return previousSolutions
+    .flatMap((solution) => nextRowColumnns
+      .map((next) => solution.indexOf(next[0]) === -1 && !isIntersectingOtherQueensDiagonal(solution, next[0]) ? solution.concat(next) : []));
+}
+
+const isIntersectingOtherQueensDiagonal = (solution: PositionsForQueens, next: Col) => {
+  const solutionLength = solution.length
+  const plop = solution.some((position, index) => {
+    return Math.abs(next - position) === (solutionLength - index)
+  })
+  return plop;
 }
 
 type Col = number;
 type PositionsForQueens = Array<Col>;
+
+
 
 
 //////////////////////////
