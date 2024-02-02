79259488de23fbbec32cd1786463acf126cf80fd
Bastien Tran <bastien.tran@comet.team>
2024-02-02 13:31:05 +0100

    two algos producing same output with valid results for nb queens ranging from 0 to 5

--- src/queens.test.ts
+++ src/queens.test.ts
@@ -1,38 +1,46 @@
 import { describe, expect, it } from "vitest";
-
+import * as NEA from 'fp-ts/NonEmptyArray'
 describe('', () => {
-  it('Should find 0 solutions for zero queens given ignoring diagonals', () => {
-
+  it('Should find 1 solutions for zero queens', () => {
     //When
-    const result = solveNQueens(0) //?
-    //Then
-    expect(result).toHaveLength(0)
-  })
-  it('Should find 1 solutions for one queens given ignoring diagonals', () => {
-
-    //When
-    const result = solveNQueens(1) //?
+    const result = solveNQueens(0)
     //Then
     expect(result).toHaveLength(1)
   })
-  it('Should find 0 solutions for two queens given ignoring diagonals', () => {
+  it('Should find 1 solutions for one queens', () => {
+
     //When
-    const result = solveNQueens(2) //?
+    const result = solveNQueens(1)
+    //Then
+    expect(result).toHaveLength(1)
+  })
+  it('Should find 0 solutions for two queens', () => {
+    //When
+    const result = solveNQueens(2)
     //Then
     expect(result).toHaveLength(0)
   })
-  it('Should find 0 solutions for three queens given ignoring diagonals', () => {
+  it('Should find 0 solutions for three queens', () => {
     //When
-    const result = solveNQueens(3) //?
+    const result = solveNQueens(3)
     //Then
     expect(result).toHaveLength(0)
   })
-  it('Should find  solutions for 4 queens given ignoring diagonals', () => {
+  it('Should find 2 solutions for four queens', () => {
     //When
-    const result = solveNQueens(4) //?
-    console.log('result: ', result);
+    const result = solveNQueens(4)
+    const result2 = solveNQueens2(4)
     //Then
     expect(result).toHaveLength(2)
+    expect(result).toEqual(expect.objectContaining(result2));
+  })
+  it('Should find 10  solutions for five queens', () => {
+    //When
+    const result = solveNQueens(5)
+    const result2 = solveNQueens2(5)
+    //Then
+    expect(result).toHaveLength(10)
+    expect(result).toEqual(expect.objectContaining(result2));
   })
   //it('should detect that two queens are on the same diagonal', () => {})
 })
@@ -42,17 +50,30 @@ const solveNQueens = (nbQueens: Col): Array<PositionsForQueens> => {
   return result;
 };
 
+const solveNQueens2 = (nbQueens: Col): Array<PositionsForQueens> => {
+  const result = generateRow2(nbQueens, nbQueens);
+  return result;
+};
+
 const generateRow = (nbQueens: Col, previousSolutions: PositionsForQueens[] = [[]]): PositionsForQueens[] => {
   const nextRowColumnns: PositionsForQueens[] = generateNextRowPositionsToTry(nbQueens);
   const validSolutionsForCurrentIteration = buildValidSolutions(previousSolutions, nextRowColumnns);
-
   if (validSolutionsForCurrentIteration.length === 0) {
-    return [];
+    return [[]].filter((solution) => solution.length === nbQueens);
   }
   if (validSolutionsForCurrentIteration.some((solution) => solution.length === nbQueens)) {
     return validSolutionsForCurrentIteration.filter((solution) => solution.length === nbQueens)
   }
-  return generateRow(nbQueens, validSolutionsForCurrentIteration)
+  return generateRow(nbQueens, validSolutionsForCurrentIteration);
+}
+
+const generateRow2 = (nbQueens: number, currentLine: number): PositionsForQueens[] => {
+  if (currentLine === 0) {
+    return [[]]
+  } else {
+    const rangeOfNextSolution =  NEA.range(0, nbQueens -1).map(item => [item]);
+    return buildValidSolutions(generateRow2(nbQueens, currentLine - 1), rangeOfNextSolution);
+  }
 }
 
 const buildValidSolutions = (previousSolutions: PositionsForQueens[], nextRowColumnns: PositionsForQueens[]) => {
@@ -76,11 +97,8 @@ const isOnSameColumnAsPreviousQueens = (solution: PositionsForQueens, next: Posi
 }
 
 const generateNextRowPositionsToTry = (nbQueens: number) => {
-  const nextRowColumnns: PositionsForQueens[] = [];
-  for (let i = 0; i < nbQueens; i++) {
-    nextRowColumnns.push([i]);
-  }
-  return nextRowColumnns;
+  const range =  NEA.range(0, nbQueens -1).map(item => [item])
+  return range
 }
 
 type Col = number;
